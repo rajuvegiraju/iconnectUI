6eaa1dd4805d51aa37c800e6e1de8e29c0df7c6d
rajuvegi <suryanarayana.raju-vegiraju@hpe.com>
2018-09-06 22:30:14 +0530

    iconnect

--- src/app/placement-officer/placement-officer.module.ts
+++ src/app/placement-officer/placement-officer.module.ts
@@ -10,12 +10,14 @@ import { IntenshipComponent } from './intenship/intenship.component';
 import { AvailablejobsComponent } from './availablejobs/availablejobs.component';
 import { TrainingsComponent } from './trainings/trainings.component';
 import { InterviewSchedulesComponent } from './interview-schedules/interview-schedules.component';
+import { ViewStudentsComponent } from './view-students/view-students.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PlacementOfficerRoutingModule
   ],
-  declarations: [StudentListComponent, TemplateComponent, AddStudentComponent, PendingApprovalComponent, IntenshipComponent, AvailablejobsComponent, TrainingsComponent, InterviewSchedulesComponent]
+  declarations: [StudentListComponent, TemplateComponent, AddStudentComponent, PendingApprovalComponent, IntenshipComponent, AvailablejobsComponent, TrainingsComponent, InterviewSchedulesComponent, ViewStudentsComponent, ProfileComponent]
 })
 export class PlacementOfficerModule { }
