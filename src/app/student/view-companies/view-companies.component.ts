178cca9bf321a869579061d1f9d38c72fdedb61d
anil pyreddy <anilpyreddy@Els-MacBook-3.local>
2018-09-23 21:43:48 +0530

    Created view company page

--- src/app/student/view-companies/view-companies.component.ts
+++ src/app/student/view-companies/view-companies.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { IconnectService } from '../../iconnect.service';
+import { DataService } from '../../datachange.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-view-companies',
@@ -6,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-companies.component.css']
 })
 export class ViewCompaniesComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'website', 'industry', 'location'];
+  dataSource: any = [];
 
-  constructor() { }
+  constructor(private _iService: IconnectService, private dataService: DataService, private router: Router) { }
 
   ngOnInit() {
+    this.dataService.navMessage.subscribe(message => {
+      this._iService.collegeList().subscribe(response => {
+        let ELEMENT_DATA = response.payload.college;
+        this.dataSource = new MatTableDataSource<any>(ELEMENT_DATA)
+      })
+    });
   }
 
 }
