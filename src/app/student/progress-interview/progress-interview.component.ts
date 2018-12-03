644ffcb9dc303742f9f25a85d5809231cc871e71
anil pyreddy <anilpyreddy@Els-MacBook-3.local>
2018-12-03 08:36:59 -0800

    Added stepper in that progress page

--- src/app/student/progress-interview/progress-interview.component.ts
+++ src/app/student/progress-interview/progress-interview.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+/**
+ * @title Stepper label bottom position
+ */
 
 @Component({
   selector: 'app-progress-interview',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress-interview.component.css']
 })
 export class ProgressInterviewComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
