7bdeede26b2a025e3b2e998625991f879b5a156b
anil pyreddy <anilpyreddy@Els-MacBook-3.local>
2019-01-15 10:51:40 -0500

    skill assignment design has done

--- src/app/placement-officer/skill-assignment/skill-assignment.component.ts
+++ src/app/placement-officer/skill-assignment/skill-assignment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { IconnectService } from '../../iconnect.service';
+import { DataService } from '../../datachange.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-skill-assignment',
@@ -6,7 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-assignment.component.css']
 })
 export class SkillAssignmentComponent implements OnInit {
-
+  skills = {};
+  displayedColumns: string[] = ['id', 'name', 'email', 'aggregate'];
+  dataSource: any = [{id:1, name:"java", email:"java@gmail.com", aggregate:"50%"}];
   constructor() { }
 
   ngOnInit() {
