c2a7098d4ae504c6810c5c816a5f92d02d8486e2
anil pyreddy <anilpyreddy@Els-MacBook-3.local>
2019-01-14 21:55:02 -0500

    Small changes done skill dev

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-    MatStepperModule, MatTooltipModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
+    MatTabsModule, MatStepperModule, MatTooltipModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
     MatInputModule, MatSlideToggleModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule,
     MatTableModule,MatCheckboxModule, MatNativeDateModule, MatDatepickerModule, MatSnackBarModule, MatCardModule, MatDialogModule, MatFormFieldModule,MatSelectModule
 } from '@angular/material';
@@ -10,12 +10,12 @@ import {
 
 @NgModule({
     imports: [
-        MatStepperModule, MatTooltipModule, CommonModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
+        MatTabsModule, MatStepperModule, MatTooltipModule, CommonModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
         MatInputModule, MatSlideToggleModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule,
         MatTableModule,MatCheckboxModule, MatDatepickerModule, MatNativeDateModule,MatSnackBarModule, MatCardModule, MatDialogModule,MatFormFieldModule,MatSelectModule
     ],
     exports: [
-        MatStepperModule, MatTooltipModule, CommonModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
+        MatTabsModule, MatStepperModule, MatTooltipModule, CommonModule, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule,
         MatInputModule, MatSlideToggleModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule,
         MatTableModule,MatCheckboxModule, MatDatepickerModule, MatSnackBarModule, MatCardModule, MatDialogModule,MatFormFieldModule,MatSelectModule
     ],
